b2219ff4b2a7d7c13241e6cbd82cdf08376a63bd
NimeshChanuka2000 <nimeshchanu1234@gmail.com>
2024-07-24 22:40:36 +0530

    create model

--- src/main/java/com/example/jwt_security_example/model/User.java
+++ src/main/java/com/example/jwt_security_example/model/User.java
@@ -1,4 +1,55 @@
 package com.example.jwt_security_example.model;
 
-public class User {
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class User implements UserDetails {
+
+    private String name;
+    private String username;
+    private String password;
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> roles = new ArrayList<>();
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
